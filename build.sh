9362fca91374977beb5647fcc267f6276c5f48b1
mohd faiz <faiz354445@gmail.com>
2026-05-02 22:05:13 +0530

    fixed build.sh final

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Install Chrome
 apt-get update
 apt-get install -y wget unzip
 
-# Chrome install
-wget https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chrome-linux64.zip
+wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chrome-linux64.zip
 unzip chrome-linux64.zip
 
-# Chromedriver install
-wget https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip
+wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
