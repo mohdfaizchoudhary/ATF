444b62d053a850253306f554b2ad463f41296b95
mohd faiz <faiz354445@gmail.com>
2026-05-03 20:37:14 +0530

    Final stable scraper fix

--- build.sh
+++ build.sh
@@ -4,5 +4,16 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Download Chrome for Testing (no apt-get needed, wget works without root)
+CHROME_VERSION="124.0.6367.91"
+
+wget -q "https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chrome-linux64.zip"
+unzip -q chrome-linux64.zip
+chmod +x chrome-linux64/chrome
+
+wget -q "https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chromedriver-linux64.zip"
+unzip -q chromedriver-linux64.zip
+chmod +x chromedriver-linux64/chromedriver
+
 # Run Django collectstatic
 python manage.py collectstatic --no-input
