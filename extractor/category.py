4fed01c9506188b19a998a4edf9d49448b2f3e4c
mohd faiz <faiz354445@gmail.com>
2026-05-03 14:38:09 +0530

    Update Chrome driver: use --headless and add --disable-gpu flag

--- extractor/category.py
+++ extractor/category.py
@@ -109,9 +109,10 @@ def scrape_gem_forcefully():
     output_file = 'gem_categories.json'
     
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
     # 🔥 UPDATED CHROME LOGIC (Render + Local)
--- extractor/utils.py
+++ extractor/utils.py
@@ -229,9 +229,10 @@ def check_states_in_pdf(pdf_url, target_states):
 
 def start_gem_scraping(category_text, states_text):
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
     prefs = {"profile.managed_default_content_settings.images": 2}
